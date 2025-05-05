c5c64d999cffb94f8da0efa1d5e790370dd15bb9
AkiNightingale <anna.solovyova.ofcl@gmail.com>
2025-05-05 19:02:15 +0200

    add configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,33 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/onboarding-form/',
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  optimizeDeps: {
-    include: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+export default defineConfig(({ command }) => (
+    {
+      plugins: [react()],
+      base: command === 'serve' ? '/' : '/onboarding-form/',
+      server: {
+        port: 3000,
+      },
+      resolve: {
+        alias: {
+          '@': '/src',
         },
       },
-    },
-  },
-})
+      optimizeDeps: {
+        include: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: true,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              react: ['react', 'react-dom'],
+              mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+            },
+          },
+        },
+      },
+    }
+  ),
+);
